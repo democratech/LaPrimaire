6b85ada95c32bc5c7d3ba701a155f0251adbb70a
Thibauld Favre <tfavre@gmail.com>
2015-06-21 09:32:44 -0400

    * ajout inline parameter implementation in php

--- web/index.php
+++ web/index.php
@@ -31,7 +31,6 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body id="page-top" class="index">
@@ -42,8 +41,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <form name="sentMessage" id="contactForm" novalidate>
-
-                        <div class="row" id="lettre_inline" style="display:none;">
+<?php if (isset($_GET["inline"])) { ?>
+                        <div class="row" id="lettre_inline">
                             <div class="col-md-9">
 				    <div class="lettre">
 					<h2>Pour de vraies elections democratiques en 2017</h2>
@@ -80,8 +79,8 @@
                             </div>
                         </div>
 
-
-                        <div class="row" id="lettre_sequence" style="display:visible;">
+<?php } else { ?>
+                        <div class="row" id="lettre_sequence">
 				    <div class="lettre">
 					<h2>Pour de vraies elections democratiques en 2017</h2>
 					<p>Cher Nicolas, Cher François, Chère Marine etc...</p>
@@ -123,6 +122,7 @@
 					</div>
 				</div>
                         </div>
+<?php } ?>
                     </form>
                 </div>
             </div>
@@ -727,8 +727,16 @@ var now = new Date();
 var countdown = (temps.getTime()-now.getTime())/1000;
 var clock = new FlipClock($('.countdown'), countdown, { countdown: true, language: 'fr', clockFace: 'DailyCounter' });
 clock.start();
-//$('#lettre_inline').remove();
+</script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60977053-1', 'auto');
+  ga('send', 'pageview');
 </script>
 
 </body>
