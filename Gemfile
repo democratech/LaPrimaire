14a4a1561b514d008ca745e2f33e5ad1e4c782ac
Thibauld Favre <tfavre@gmail.com>
2015-08-10 14:08:36 -0400

    v1.0 of democratech website

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+# Static site generator
+gem 'nanoc', '~> 3.7.5'
+gem 'adsf', '~> 1.2.0'
+
+# SASS & cool stuff
+gem 'compass', '~> 1.0.3'
+gem 'bootstrap-sass', '~> 3.3.3'
+gem 'font-awesome-sass', '~> 4.3.0'
+
+# Guard
+gem 'guard-nanoc', '~> 1.0.3'
+gem 'rb-readline', '~> 0.5.2'
+gem 'uglifier', '>= 1.0.3'
+gem 'rainpress'
+gem 'nanoc-cachebuster'
---
+++ content/assets/scripts/fb_ga_tw.js
@@ -0,0 +1,45 @@
+/* Facebook */
+window.fbAsyncInit=function() {
+	FB.init({
+			appId : '115809635110050',
+			xfbml : true,
+			version : 'v2.3'
+		});
+	FB.Event.subscribe('edge.create',function(targetUrl) {
+			ga('send', 'social', 'facebook', 'like',{'eventCategory':'facebook_action','eventAction':'like','eventLabel':targetUrl});
+	});
+};
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/* Google Analytics */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-60977053-1', 'auto');
+ga('send', 'pageview');
+ga('send', 'event','signature','read',{'nonInteraction' : 1});
+
+/* Twitter */
+window.twttr = (function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0],
+t = window.twttr || {};
+if (d.getElementById(id)) return t;
+js = d.createElement(s);
+js.id = id;
+js.src = "https://platform.twitter.com/widgets.js";
+fjs.parentNode.insertBefore(js, fjs);
+
+t._e = [];
+t.ready = function(f) {
+t._e.push(f);
+};
+return t;
+}(document, "script", "twitter-wjs"));
+
