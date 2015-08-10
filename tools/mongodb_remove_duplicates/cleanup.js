d7e2146451262c03069611060e36522bdc86f8d3
Thibauld Favre <tfavre@gmail.com>
2015-08-10 16:32:21 -0400

    Add mongodb cleanup script

---
+++ tools/mongodb_remove_duplicates/cleanup.js
@@ -0,0 +1,10 @@
+db.supporteurs.remove({"created": {"$lt": ISODate("2015-06-23T06:05:51Z")}})
+var keys = {};
+db.supporteurs.find().forEach(function(doc) {
+	var key=doc.email;
+	if (keys[key]) {
+		db.supporteurs.remove({_id: doc._id});
+	} else {
+		keys[key] = true;
+	}
+});
