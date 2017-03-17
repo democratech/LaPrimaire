18a30cb6785d292e1af858a2715c0a4b198c10be
Thibauld Favre <tfavre@gmail.com>
2017-03-17 10:25:19 -0400

    MAJ home page

--- content/assets/scripts/goalProgress.js
+++ content/assets/scripts/goalProgress.js
@@ -13,9 +13,12 @@
 				goalAmount: 100,
 				currentAmount: 50,
 				indicatorAmount: 0,
+				promessesAmount: 0,
 				speed: 1000,
 				textBefore: '',
 				textAfter: '',
+				textBeforeIndicator: '',
+				textAfterIndicator: '',
 				milestoneNumber: 70,
 				milestoneClass: 'almost-full',
 				callback: function() {}
@@ -31,6 +34,9 @@
 
 				// Calculate size of the progress bar
 				var percentage = (currentAmountParsed / goalAmountParsed) * 100;
+				if (percentage>100) {
+					percentage=100;
+				}
 				var percentage_str = Math.round(percentage);
 				var milestoneNumberClass = (percentage > defaults.milestoneNumber) ? ' ' + defaults.milestoneClass : '';
 
@@ -41,8 +47,18 @@
 				if (defaults.indicatorAmount!=0) {
 					var indicatorAmountParsed = parseInt(defaults.indicatorAmount);
 					var percentageInd = (1-(indicatorAmountParsed / goalAmountParsed)) * 100;
+					//var indicatorBar = '<div class="indicatorBar">'+ defaults.textBeforeIndicator + indicatorAmountParsed + defaults.textAfterIndicator+'</div>';
 					var indicatorBar = '<div class="indicatorBar"></div>';
 					progressBarWrapped = '<div class="goalProgress' + milestoneNumberClass + '">' + progressBar + indicatorBar + '</div>';
+				}
+				if (defaults.promessesAmount!=0) {
+					var promessesAmountParsed = parseInt(defaults.promessesAmount);
+					var percentagePro = (promessesAmountParsed / goalAmountParsed) * 100;
+					if (percentagePro>100) {
+						percentagePro=100;
+					}
+					var promesseBar = '<div class="indicatorBar">'+ defaults.textBeforeIndicator + promessesAmountParsed + defaults.textAfterIndicator+'</div>';
+					progressBarWrapped = '<div class="goalProgress' + milestoneNumberClass + '">' + progressBar + promesseBar + '</div>';
 				}
 
 				// Append to the target
@@ -61,6 +77,11 @@
 					var renderedInd = obj.find('div.indicatorBar');
 					renderedInd.each(function() { $(this).html($(this).text().replace(/\s/g, '&nbsp;')); });
 					renderedInd.animate({width: percentageInd +'%'}, defaults.speed, defaults.callback);
+				}
+				if (defaults.promessesAmount!=0) {
+					var renderedPro = obj.find('div.indicatorBar');
+					renderedPro.each(function() { $(this).html($(this).text().replace(/\s/g, '&nbsp;')); });
+					renderedPro.animate({width: percentagePro +'%'}, defaults.speed, defaults.callback);
 				}
 
 				if(typeof callback == 'function') {
